67a22916f005ebf05a34ea0c4bc3a0bb032fdb42
jnrtgrsggv <135415881+jnrtgrsggv@users.noreply.github.com>
2024-10-01 23:24:39 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || 'fdf7b399-782c-4b6a-84c3-5e813cbec860'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'N9BVoBfucVIrIzCBt8';   // 没用哪吒，把这个参数空着
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'mfutnjdrxdj.rootjfennewdonmain.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNWVmN2IzYzc2YmExMWFjOWY3MzYwYTcxYmEzZTgwMzIiLCJ0IjoiYWQ2YjBlNDEtMmMzNC00OGFhLThiNmEtYmI2MzEwNTZlYzEzIiwicyI6IlpXUXpaR1pqWVRrdFl6aGpNaTAwTXpOa0xUazBaamt0T0RCaVpqY3paakZtWm1aaSJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'nndrty456vg.rootjfennewdonmain.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNWVmN2IzYzc2YmExMWFjOWY3MzYwYTcxYmEzZTgwMzIiLCJ0IjoiMmI1NmUxYzktYmViMC00NWM3LWE0ZTUtMmQ4MDIwOGYyYTUxIiwicyI6Ik1qWTBZV0l6TWpjdE56QTFOUzAwWkRRekxUbGpOakF0WlRJMk1HSTFNR0l3TjJFMiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
